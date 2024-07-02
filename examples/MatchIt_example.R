6717432a5341cde10697fdb996211a568094ab3e
Lbreidenbach <lindsay.b.breidenbach@vanderbilt.edu>
2024-07-02 16:59:42 -0500

    Changed DAG name to highlight purpose of adjusting for confounders

--- examples/MatchIt_example.R
+++ examples/MatchIt_example.R
@@ -1,16 +1,16 @@
 #creates the causal diagram
-match_dag = HydeNetwork(~B|A + C|A + D|B*C)
+confounder_dag = HydeNetwork(~B|A + C|A + D|B*C)
 
 #adds set effect sizes and distributions to the causal diagram
-match_dag = setNode(my_dag, A, nodeType = "dbern", prob = 0.5)
-match_dag = setNode(my_dag, B, nodeType = "dnorm", mu = paste0(1," * A + ",0 - 1*0.5), tau = 1)
-match_dag = setNode(my_dag, C, nodeType = "dbern", prob = paste0("ilogit(",0.8," * A + ",set_p(0.1, 0.8*0.5),")"))
-match_dag = setNode(my_dag, D, nodeType = "dbern", prob = paste0("ilogit(",0.05," * B + ", 1.2," * C + ",set_p(0.25, 1.2 * 0.1 + 0.05 * 0 ),")"))
+confounder_dag = setNode(my_dag, A, nodeType = "dbern", prob = 0.5)
+confounder_dag = setNode(my_dag, B, nodeType = "dnorm", mu = paste0(1," * A + ",0 - 1*0.5), tau = 1)
+confounder_dag = setNode(my_dag, C, nodeType = "dbern", prob = paste0("ilogit(",0.8," * A + ",set_p(0.1, 0.8*0.5),")"))
+confounder_dag = setNode(my_dag, D, nodeType = "dbern", prob = paste0("ilogit(",0.05," * B + ", 1.2," * C + ",set_p(0.25, 1.2 * 0.1 + 0.05 * 0 ),")"))
 
 #create data creates a single dataset.
 #This lets users see if the simulated datasets behave as expected before committing to a large simulation with many iterations
 
-sim_data = create_data(match_dag, 10000)
+sim_data = create_data(confounder_dag, 10000)
 lm(B~C, sim_data)
 
 matched_data = matchit_matching("C", "A", sim_data, ratio = 6)
