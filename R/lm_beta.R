fc2b6f076e9e233f6a4da26cc4478c70294a6cd0
Lbreidenbach <lindsay.b.breidenbach@vanderbilt.edu>
2024-07-03 16:34:29 -0500

    initial commit

---
+++ R/lm_beta.R
@@ -0,0 +1,43 @@
+#'Analyzes a data frame to give an odds ratio
+#'
+#'runs a linear regression of with the following parameters order like so: outcome ~ exposure + covariates.
+#'
+#'
+#' @param exposure Character value. The name of a column in a data frame that represents exposure in an
+#'
+#' @param outcome Character value. The name of a \strong{Gaussian} column in a data frame that represents the outcome.
+#'
+#' @param covariates Character value/vector. The name(s) of the columns that the user wants adjust on.
+#'
+#' @param df The data frame for analysis
+#'
+#' @return A six column data frame with the following summary statistics:
+#' * odds ratio (reads NA)
+#' * beta coeffecient
+#' * lower 95% confidence interval of beta coefficient
+#' * upper 95% confidence interval of beta coefficient
+#' * p value
+#' * number of samples in the dataset
+#'
+#'@examples McBias/examples/effect_estimate_functions.R
+#'@seealso [varied_runs()],[apply_methods()]
+#'@export
+#'
+
+lm_beta = function(exposure, outcome, covariates=NULL, df){
+  vars = c(exposure, covariates)
+  lm1 = stats::lm(as.formula(paste(outcome, paste(vars, collapse=" + "), sep=" ~ ")), data = df)
+  confint = confint(lm1, trace = F)
+  upper_int = confint[2,2]
+  lower_int = confint[2,1]
+  beta = as.numeric(lm1$coefficients[2])
+  regression_df = data.frame("odds_ratio" = NA,
+                             beta = beta,
+                             lower_int =lower_int,
+                             upper_int = upper_int,
+                             confint_diff = abs(upper_int-lower_int),
+                             p_val = coef(summary(lm1))[2,4],
+                             n = nrow(df))
+  rownames(regression_df) = "linear_regression"
+  return(regression_df)
+}
---
+++ R/ps_weight.R
@@ -0,0 +1,60 @@
+#'Analyzes a data frame to give an odds ratio
+#'
+#'runs a linear or logistic regression depending on if the outcome is Gaussian or binary. Adjusts for covariates with given weights
+#'The regression is created with the following parameters ordered like so: outcome ~ exposure + covariates.
+#'
+#'
+#' @param exposure Character value. The name of a column in a data frame that represents exposure in an
+#'
+#' @param outcome Character value. The name of a column in a data frame that represents the outcome.
+#'
+#' @param covariates Character value/vector. The name(s) of the columns that the user wants adjust on.
+#'
+#' @param df The data frame for analysis. must have a column that includes weights. Users may generate a weight column with get_ps()
+#'
+#' @param weights Character value. The name of a column in a data frame that represents the weights the user wishes to use.
+#'
+#' @return A six column data frame with the following summary statistics:
+#' * odds ratio (reads NA if outcome is Gaussian)
+#' * beta coeffecient
+#' * lower 95% confidence interval of beta coefficient
+#' * upper 95% confidence interval of beta coefficient
+#' * p value
+#' * number of samples in the dataset
+#'
+#'@examples McBias/examples/effect_estimate_functions.R
+#'@seealso [varied_runs()],[apply_methods()], [get_ps()]
+#'@export
+#'
+
+ps_weight = function(exposure, outcome, covariates, df, weights){
+  vars = c(exposure, covariates)
+  if(class(df[,outcome]) == "numeric" ){
+    cont_glm = stats::lm(as.formula(paste(outcome, paste(vars, collapse=" + "), sep=" ~ ")), data = df, weights = weights)
+  }else if(class(df[,outcome]) == "integer"){
+    cont_glm = stats::glm(as.formula(paste(outcome, paste(vars, collapse=" + "), sep=" ~ ")), data = df, weights = weights, family = "quasibinomial")
+  }else{
+    warning("exposure must be numeric or integer")
+  }
+
+  exp_coef = as.numeric(cont_glm$coefficients[2])
+  exp_or = exp(exp_coef)
+  or_confint = stats::confint.default(cont_glm, trace = F)
+  or_upper = or_confint[2,2]
+  or_lower = or_confint[2,1]
+  int_diff = or_upper - or_lower
+  if(class(df[,outcome]) == "numeric" ){
+    exp_or = NA
+  }
+
+
+  or_df = data.frame("odds_ratio" = exp_or,
+                     beta = exp_coef,
+                     lower_int = or_lower,
+                     upper_int = or_upper,
+                     confint_diff = abs(int_diff),
+                     p_val = coef(summary(cont_glm))[2,4],
+                     n = nrow(df))
+  row.names(or_df) = "ps_weighting"
+  return(or_df)
+}
